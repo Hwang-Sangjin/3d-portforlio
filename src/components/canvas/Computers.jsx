c07790022ab1ead73f3443678b63a6499f178c69
Sangjin Hwang <aosdb5640@naver.com>
2023-09-20 22:25:05 +0900

    add: load 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,18 +10,32 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} />
-    </mesh>
+    <hemisphereLight intensity={0.15} groundColor='black' />
+    <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}
+    />
+    <pointLight intensity={10} />
+    <primitive
+      object={computer.scene}
+      scale={ 0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+    />
+  </mesh>
   );
 };
 
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -31,10 +45,11 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers  />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
 };
-export default Computers;
+export default ComputersCanvas;
